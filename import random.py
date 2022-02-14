d386acebb40753db36e09e48ca4a0c52cf2a05c2
Sujay Kanakamedala <rkanakamedala@gmail.com>
2022-02-13 19:19:35 -0800

    Homework 3-1 assignmnet 5

--- import random.py
+++ import random.py
@@ -1,14 +1,13 @@
-h = int(input('Enter x Value '))
-u = int(input('Enter y Value '))
-if h>0 and u>0:
-print ("Quadrant 1")
-elif h>0 and u<0:
-print ("Quadrant 4")
-elif h<0 and u<0:
-print ("Quadrant 3")
-elif h<0 and u>0:
-print ("Quadrant 2")
-elif h==0:
-print ("N/A")
-elif u==0:
-print ("N/A")
+import random
+n = random.randint(0, 100)
+print (n)
+u= random.randint(0, 100)
+print (u)
+m= random.randint(0, 100)
+print (m)
+if m==n==u:
+print ("All Same", n,u,m)
+elif m==n or n==u or m==u:
+print ("Two are the same", m, n, u)
+elif m!=n or n!=u or m!=u:
+print ("None are the same", m, n, u)
