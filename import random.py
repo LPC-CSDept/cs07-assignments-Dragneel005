5e345d1370d66ec787897034b122ee788f2a48cb
Sujay Kanakamedala <rkanakamedala@gmail.com>
2022-02-13 19:17:06 -0800

    Homework 3-1 assignment 4

--- import random.py
+++ import random.py
@@ -1,25 +1,14 @@
-import random
-n = random.randint(0, 100)
-print (n)
-u= random.randint(0, 100)
-print (u)
-m= random.randint(0, 100)
-print (m)
-if n<u and n<m:
-  print ("The Smallest Number is ", n)
-elif n==u and n<m:
-  print ("The Smallest Numbers are ", n,u)
-elif n==m and n<u:
-  print ("The Smallest Numbers are ", n, m)
-if u<n and u<m:
-  print ("The Smallest Number is ", u)
-elif u==n and u<m:
-  print ("The Smallest Numbers are ", u, n)
-elif u==m and u<n:
-  print("The Smallest Numbers are ", u, m)
-if m<n and m<u:
-  print ("The Smallest Number is ", m)
-elif m==n and m<u:
-  print("The Smallest Numbers are", m, n)
-elif m==u and m<n:
-  print("The Smallest Numbers are ", m, u)
+h = int(input('Enter x Value '))
+u = int(input('Enter y Value '))
+if h>0 and u>0:
+print ("Quadrant 1")
+elif h>0 and u<0:
+print ("Quadrant 4")
+elif h<0 and u<0:
+print ("Quadrant 3")
+elif h<0 and u>0:
+print ("Quadrant 2")
+elif h==0:
+print ("N/A")
+elif u==0:
+print ("N/A")
