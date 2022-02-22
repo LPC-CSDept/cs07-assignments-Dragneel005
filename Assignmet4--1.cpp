8ae4ce88995df3341d3407df17adf9688bf4bb9a
Sujay Kanakamedala <98435886+Dragneel005@users.noreply.github.com>
2022-02-21 18:59:48 -0800

    Add files via upload

---
+++ Assignmet4--1.cpp
@@ -0,0 +1,21 @@
+for i in range (1,11):
+  print(2**i, end=',')
+
+
+
+sum = 0
+for i in range (5):
+  num= int(input('Enter your number'))
+  sum += num
+print("The sum is", sum)
+
+
+
+
+sum=0
+i =0
+while (i < 5):
+  n = int(input('Enter your number'))
+  sum += n
+  i += 1
+print ('Total {0}'.format(sum))
