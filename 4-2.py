4614bc301738b76d216edd1c210204384dee1aca
Sujay Kanakamedala <98435886+Dragneel005@users.noreply.github.com>
2022-02-22 18:32:20 -0800

    Add files via upload

---
+++ 4-2.py
@@ -0,0 +1,13 @@
+i = 1
+while ( i<11):
+	print (i, end='\t')
+	i += 1
+
+
+
+i=input('Enter a value= ')
+print ("You have entered {0}".format(i))
+while (i!= 'q'):
+  i=input('Enter a value= ')
+  print ("You have entered {0}".format(i))
+print("You have entered q the loop is now over")
