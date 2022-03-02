73a12a0fae6909cc4c787c93a931b47778615034
Sujay Kanakamedala <98435886+Dragneel005@users.noreply.github.com>
2022-03-01 21:46:10 -0800

    Add files via upload

---
+++ Assignment 4-4.py
@@ -0,0 +1,28 @@
+import random
+pre=random.randint(0,100)
+while True:
+  cn=random.randint(0,100)
+  print("The prevous number is {0} and the current number is {1}" .format (pre, cn))
+  if (pre <= cn):
+    print ('The current number {0} is greater than the prevous number {1}' .format(cn, pre))
+    break
+
+
+
+
+import random
+i=0
+num=0
+while (i<10):
+  nums=random.randint(0,100)
+  print (nums, end=',')
+  nums+=num
+  if (num>100):
+    print("The total is {0}".format(num))
+    break
+  i += 1
+
+
+for i in range(0,3):
+	for j in range(i,3):
+		print (j, i)
