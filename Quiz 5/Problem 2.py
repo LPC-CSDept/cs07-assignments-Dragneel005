4072de1f9f65019599a992af282cf398279ad293
Sujay Kanakamedala <rkanakamedala@gmail.com>
2022-03-22 21:42:53 -0700

    Test

--- Quiz 5/Problem 2.py
+++ Quiz 5/Problem 2.py
@@ -1,27 +1,38 @@
-import matplotlib.pyplot as plt
-import numpy as np
+U=int(input('Input number of scores'))
+Bill=[]
+for i in range (U):
+  bill=int(input("Enter Bill's scores"))
+  Bill.append(bill)
 
-label = ['Bill', 'Jim', 'Joe']
-width=0.15
-x=np.arange(3)
-math = [100, 100, 100]
-english = [90, 90, 80]
-physics = [90, 80, 90]
-computer = [90, 80, 90]
+Jim=[]
+for i in range (U):
+  jim=int(input("Enter Jim's scores"))
+  Jim.append(jim)
+
+Joe=[]
+for i in range (U):
+  joe=int(input("Enter Joe's scores"))
+  Joe.append(joe)
+
+score=[Bill]
+scorej=[Jim]
+scores = [Joe]
+print(score, scorej, scores, sep="\n")
 
 
-fig, ax = plt.subplots()
-p = ax.bar(x-width*1.5, math, width, label = 'Math')
-r = ax.bar(x-width*0.5, english, width, label = 'English')
-q = ax.bar(x+width*0.5, physics, width, label = 'Physics')
-s = ax.bar(x+width*1.5, computer, width, label = 'Computer')
+x=sum(Bill)
+y=x/len(Bill)
+print("The sumation of Bill's score is=",x )
+print("The average of Bill's scores is=", y)
 
-ax.legend(loc = 'lower center')
-ax.set_title('Graph of scores')
-ax.set_xticks(x)
-ax.set_xticklabels(label)
-ax.bar_label(p, padding=1)
-ax.bar_label(r, padding=4)
-ax.bar_label(q, padding=4)
-ax.bar_label(s, padding=4)
-plt.show()
+
+a=sum(Jim)
+s=a/len(Jim)
+print("The sumation of Jim's score is=",a )
+print("The average of Jim's scores is=", s)
+
+
+d=sum(Joe)
+f=d/len(Joe)
+print("The sumation of Joe's score is=",d )
+print("The average of Joe's scores is=", f)
