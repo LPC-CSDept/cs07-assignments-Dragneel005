63a93b7aac892e4dfb1af47bdc64de67ffc3fc09
Sujay Kanakamedala <rkanakamedala@gmail.com>
2022-03-29 19:16:47 -0700

    redo

--- Ch 4.py/4-4 redo (9 & 11).py
+++ Ch 4.py/4-4 redo (9 & 11).py
@@ -10,4 +10,16 @@ while True:
   pre = cn
 
 
-  
+#11
+for i in range(1):
+    for a in range(1):
+        print(i , a , end=' | ')
+    print( )
+for i in range(1, 2):
+    for a in range(2):
+        print(i , a , end=' | ')
+    print( )
+for i in range(2 , 3):
+    for a in range(0 , 3):
+        print(i , a , end=' | ')
+    print( )
